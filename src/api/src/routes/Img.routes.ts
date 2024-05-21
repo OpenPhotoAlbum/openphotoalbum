63fd2e2895898eb20e1c51b60bd5b5fdc82297c0
Stephen <stephen@theyoungs.cloude>
2024-05-20 20:04:57 -0400

    Added route to fetch exif data for image

--- src/api/src/routes/Img.routes.ts
+++ src/api/src/routes/Img.routes.ts
@@ -1,7 +1,9 @@
 import express, { Request, Response } from 'express';
+import fs from 'fs';
 import dotenv from "dotenv";
 import { image } from 'src/types/Image.types';
-import { getImageBuffer } from 'src/resolvers';
+import { getImageBuffer, scanAndExportUploadedImage } from 'src/resolvers';
+import Media from 'src/media-exif';
 
 dotenv.config({ path: '/home/openphoto/config/.env.local' });
 
@@ -25,4 +27,23 @@ router.get('/img*', async (req: Request, res: Response) => {
     }
 });
 
+router.get('/meta*', async (req: Request, res: Response) => {
+    try {
+        const image_path = image(decodeURI(req.path.replace('/meta', '')));
+        const media = new Media({ path: image_path });
+
+        if (!fs.existsSync(media.json_scan_file)) {
+            await scanAndExportUploadedImage(image_path);
+        }
+
+        const data = fs.readFileSync(media.json_scan_file, { encoding: 'utf-8' });
+
+        return res
+            .status(200)
+            .json(JSON.parse(data));
+    } catch (e) {
+        console.error(e);
+        res.status(404).send()
+    }
+});
 export default router;
